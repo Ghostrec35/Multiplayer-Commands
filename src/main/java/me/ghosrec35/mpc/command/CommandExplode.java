dffd883cd0af071a172da63023add17794c68240
Alex <alexrusso225@gmail.com>
2014-05-10 00:14:37 -0400

    Even More Commands

---
+++ src/main/java/me/ghosrec35/mpc/command/CommandExplode.java
@@ -0,0 +1,34 @@
+package me.ghosrec35.mpc.command;
+
+import net.minecraft.command.CommandBase;
+import net.minecraft.command.ICommandSender;
+import net.minecraft.entity.player.EntityPlayer;
+
+public class CommandExplode extends CommandBase
+{
+    @Override
+    public String getCommandName()
+    {
+        return "explode";
+    }
+
+    @Override
+    public String getCommandUsage(ICommandSender var1)
+    {
+        return "explode.commands.usage";
+    }
+
+    @Override
+    public void processCommand(ICommandSender sender, String[] params)
+    {
+        if(canCommandSenderUseCommand(sender))
+        {
+            EntityPlayer commandUser = getCommandSenderAsPlayer(sender);
+            if(params.length == 1)
+            {
+                EntityPlayer target = commandUser.worldObj.getPlayerEntityByName(params[0]);
+                target.worldObj.createExplosion(target, target.posX, target.posY, target.posZ, 4.0f, true);
+            }
+        }
+    }
+}
---
+++ src/main/java/me/ghosrec35/mpc/command/CommandIgnite.java
@@ -0,0 +1,34 @@
+package me.ghosrec35.mpc.command;
+
+import net.minecraft.command.CommandBase;
+import net.minecraft.command.ICommandSender;
+import net.minecraft.entity.player.EntityPlayer;
+
+public class CommandIgnite extends CommandBase
+{
+    @Override
+    public String getCommandName()
+    {
+        return "ignite";
+    }
+
+    @Override
+    public String getCommandUsage(ICommandSender var1)
+    {
+        return "ignite.commands.usage";
+    }
+
+    @Override
+    public void processCommand(ICommandSender sender, String[] params)
+    {
+        if(canCommandSenderUseCommand(sender))
+        {
+            EntityPlayer commandUser = getCommandSenderAsPlayer(sender);
+            if(params.length == 2)
+            {
+                EntityPlayer target = commandUser.worldObj.getPlayerEntityByName(params[0]);
+                target.setFire(Integer.parseInt(params[1]));
+            }
+        }
+    }
+}
