08efab7a244d22ebcdf569f8beca36a190170cce
Alex <alexrusso225@gmail.com>
2014-08-10 18:01:16 -0400

    Cleaning the slate

--- src/main/java/net/epoxide/mpc/ref/Reference.java
+++
@@ -1,8 +0,0 @@
-package net.epoxide.mpc.ref;
-
-public class Reference
-{
-    public static final String MOD_ID = "MultiplayerCommands";
-    public static final String MOD_NAME = "Multiplayer Commands";
-    public static final String VERSION = "0.0.0.2-SNAPSHOT";  
-}
